65cf3a0cc36fb4cd1db8efa7ca4d7ca653e68f97
Zimizig <ramazanow9999@gmail.com>
2024-07-10 16:41:43 +0500

    Initial Commit

---
+++ MydaysApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MydaysApp
+//
+//  Created by Роман on 10.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
