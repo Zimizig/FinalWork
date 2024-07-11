5310e9a5cc8b5210cde5fb8b6f16f64737282454
Zimizig <ramazanow9999@gmail.com>
2024-07-11 13:24:24 +0500

    Finished work

--- MydaysApp/ViewController.swift
+++ MydaysApp/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var returnInfoLabel: UILabel!
+    @IBOutlet weak var returnResultButton: UIButton!
+    
+    private var numberOfDays = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        returnResultButton.layer.cornerRadius = 12
     }
 
-
+    @IBAction func UIInputDateInfo(_ sender: UIDatePicker) {
+        let range = sender.date..<Date.now
+        numberOfDays = range.formatted(.components(style: .wide, fields: [.day]))
+        numberOfDays.removeLast(5)  // Строка необходимая для удаления стандартного "Days"
+        
+    }
+    @IBAction func ReturnResultButtonTapped() {
+        
+        returnInfoLabel.text = "Ты живешь уже \(numberOfDays) дней"
+        
+    }
+    
 }
 
